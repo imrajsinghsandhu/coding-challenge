a66df3eb7d77e03730250e33a47ddb1c61f5c19c
imraj singh sandhu <e0543951@u.nus.edu>
2024-09-10 15:08:24 +0800

    Revert "Update app.py"
    This reverts commit 8d0635b9865a5c5b270f9306fcca2a3aa521b242.

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ logger = logging.getLogger(__name__)
 @app.route('/', methods=['GET'])
 def default_route():
     logging.info("Starting application ...")
-    print("GYATTTT requesttt")
     return 'Python Template'
 
 
